d8475bad4fa01f2d1afbfc5eda530a324e885718
duckida <duckidaweb@gmail.com>
2026-06-28 16:18:37 +0100

    Add icon and get progress bar to work

--- __init__.py
+++ __init__.py
@@ -1,24 +1,25 @@
+import time
+
 import wifi
 
 
 def connect_wifi():
     wifi.connect()
     while not wifi.is_connected():
-        sleep(0.2)
-
-
-# def init():  # calls when the app opens
-#    connect_wifi()
+        time.sleep(0.2)
 
 
 class ProgressBar:
     RADIUS = 4
     OUTLINE_THICKNESS = 2
+    PADDING = 4
 
-    def __init__(
-        self, top_left_corner_x, top_left_corner_y, width, height, initial_progress
-    ):
+    def __init__(self, top_left_corner_x, top_left_corner_y, width, height):
         self.fillable_width = width
+        self.top_left_x = top_left_corner_x
+        self.top_left_y = top_left_corner_y
+        self.bar_height = height
+
         # Draw the filled white inside
         outline_fill = shape.rounded_rectangle(
             top_left_corner_x,
@@ -36,28 +37,38 @@ class ProgressBar:
         screen.shape(outline)
 
         # The actual progress bar
-        self.bar_shape = shape.rounded_rectangle(
+        self.bar_shape = shape.rectangle(
             top_left_corner_x,
             top_left_corner_y,
-            self.fillable_width * initial_progress,
-            height,
-            self.RADIUS,
+            0,
+            self.bar_height,
         )
         screen.shape(self.bar_shape)
 
     def set_progress(self, progress):
         bar_width = self.fillable_width * progress
-        transformation = mat3().scale(bar_width, 1)
-        self.bar_shape.transform = transformation
+        self.bar_shape = shape.rounded_rectangle(
+            self.top_left_x,
+            self.top_left_y,
+            bar_width,
+            self.bar_height,
+            self.RADIUS,
+        )
+
+        screen.pen = color.black
+        screen.shape(self.bar_shape)
 
 
-progress = 1
-bar = ProgressBar(5, 5, 100, 10, progress)
+progress = 0.1
+bar = ProgressBar(15, 15, 200, 20)
 
 
 def update():  # runs in a loop when the app is open
     global progress
-    progress += 0.01
+    progress += 0.1
     bar.set_progress(progress)
 
     display.update()
+
+
+run(update)
